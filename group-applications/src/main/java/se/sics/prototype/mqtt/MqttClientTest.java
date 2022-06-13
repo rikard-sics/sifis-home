66a0b41d00e9580a5adcd7c1980f0a703fffb7df
Rikard Höglund <rikard.hoglund@ri.se>
2022-06-13 16:28:33 +0200

    Working test code for TLS subscription to Mosquitto test broker

--- group-applications/src/main/java/se/sics/prototype/mqtt/MqttClientTest.java
+++ group-applications/src/main/java/se/sics/prototype/mqtt/MqttClientTest.java
@@ -1,12 +1,13 @@
 package se.sics.prototype.mqtt;
 
-import org.eclipse.paho.mqttv5.client.MqttAsyncClient;
-import org.eclipse.paho.mqttv5.common.MqttException;
-
 public class MqttClientTest {
 
-	public static void main(String[] args) throws MqttException {
-		MqttAsyncClient testClient = new MqttAsyncClient("tcp://" + "localhost" + ":1883", "test");
+	public static void main(String[] args) throws Exception {
+
+		
+		BasicSSLTest.setUpBeforeClass();
+		BasicSSLTest.testSSL();
+		
 		System.out.println("HELLO");
 	}
 
