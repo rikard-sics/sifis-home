9a739046ffd1bb9f5a7209b5b81bb2b015956986
Rikard Höglund <rikard.hoglund@ri.se>
2022-07-04 17:29:35 +0200

    Test fixing

--- ace/src/test/java/se/sics/ace/coap/oscoreProfile/TestOscoreIntrospection.java
+++ ace/src/test/java/se/sics/ace/coap/oscoreProfile/TestOscoreIntrospection.java
@@ -35,8 +35,8 @@ import java.util.Map;
 
 import org.eclipse.californium.oscore.OSCoreCtx;
 import org.eclipse.californium.oscore.OSCoreCtxDB;
-import org.junit.AfterClass;
-import org.junit.BeforeClass;
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 
 import com.upokecenter.cbor.CBORObject;
@@ -96,7 +96,7 @@ public class TestOscoreIntrospection {
     /**
      * This sets up everything for the tests including the server
      */
-    @BeforeClass
+    @Before
     public static void setUp() {
         ctxDB = new org.eclipse.californium.oscore.HashMapCtxDB();
     	
@@ -108,7 +108,7 @@ public class TestOscoreIntrospection {
      * Deletes the test DB after the tests
      * @throws Exception 
      */
-    @AfterClass
+    @After
     public static void tearDown() throws Exception {
         srv.stop();
     }
