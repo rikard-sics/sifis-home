d1e0484c3369d4cf330d4c2173195486af04d3f0
Rikard Höglund <rikard.hoglund@ri.se>
2022-10-03 15:59:10 +0200

    JSON (de)serialization support

---
+++ group-applications/src/main/java/se/sics/prototype/json/JsonParseTest.java
@@ -0,0 +1,56 @@
+package se.sics.prototype.json;
+
+import java.net.URI;
+import java.util.Map;
+
+import javax.websocket.ContainerProvider;
+import javax.websocket.WebSocketContainer;
+
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
+import se.sics.prototype.json.incoming.JsonIn;
+import se.sics.prototype.json.outgoing.JsonOut;
+import se.sics.prototype.json.outgoing.OutValue;
+import se.sics.prototype.json.outgoing.RequestPubMessage;
+
+public class JsonParseTest {
+
+	/*
+	 * {"Volatile":{"value":{"message":"hi","topic":"command_dev1"}}}
+	 */
+	// https://github.com/google/gson/blob/master/UserGuide.md#maps-examples
+	// https://stackoverflow.com/a/19177892
+	// https://github.com/google/gson/blob/master/UserGuide.md#object-examples
+
+	public static void main(String[] args) throws Exception {
+		// Deserialize incoming
+
+		String test1 = "{\"Volatile\":{\"value\":{\"message\":\"hi\",\"topic\":\"command_dev1\"}}}";
+		String test2 = "{\"value\":{\"message\":\"hi\",\"topic\":\"command_dev1\"}}";
+		String test3 = "{\"key\": \"value\"}";
+
+		Gson gson = new Gson();
+		JsonIn parsed = gson.fromJson(test1, JsonIn.class);
+
+		System.out.println(parsed.getVolatile().getValue().getTopic());
+		System.out.println(parsed.getVolatile().getValue().getMessage());
+
+		// Serialize outgoing
+
+		// {"RequestPubMessage":{"value":{"message":"hi","topic":"output_dev1"}}}
+
+		JsonOut outgoing = new JsonOut();
+		RequestPubMessage pubMsg = new RequestPubMessage();
+		OutValue outVal = new OutValue();
+		outVal.setTopic("output_dev1");
+		outVal.setMessage("test123");
+		pubMsg.setValue(outVal);
+		outgoing.setRequestPubMessage(pubMsg);
+
+		Gson gsonOut = new Gson();
+		String jsonOut = gsonOut.toJson(outgoing);
+
+		System.out.println("Outgoing JSON: " + jsonOut);
+	}
+}
